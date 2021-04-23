785db4ee45b7449dc32463599e7df8d8f36f48fd
Bionic Ape <51401866+BionicApe@users.noreply.github.com>
2021-04-23 15:57:20 +0200

    Module Template

---
+++ TemplateModuleName/Private/TemplateModuleNameModule.cpp
@@ -0,0 +1,22 @@
+// Created by Bionic Ape. All Rights Reserved.
+
+#include "TemplateModuleName.h"
+
+#define LOCTEXT_NAMESPACE "FTemplateModuleNameModule"
+
+DEFINE_LOG_CATEGORY(LogTemplateModuleName);
+
+void FTemplateModuleNameModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FTemplateModuleNameModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FTemplateModuleNameModule, TemplateModuleName)
