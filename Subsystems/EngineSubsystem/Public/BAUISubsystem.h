af569a983d954f444ea2ff9c33b1fd01f4e3aafc
Bionic Ape <51401866+BionicApe@users.noreply.github.com>
2021-05-12 12:13:44 +0200

    EngineSubsystem

---
+++ Subsystems/EngineSubsystem/Public/BAUISubsystem.h
@@ -0,0 +1,17 @@
+// Created by Bionic Ape. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/EngineSubsystem.h"
+#include "BAUISubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIONICAPEUI_API UBAUISubsystem : public UEngineSubsystem
+{
+	GENERATED_BODY()
+	
+};
