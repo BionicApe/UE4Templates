0dfcb8d44ad6b6027715747b3dd34ad197e37520
Bionic Ape <51401866+BionicApe@users.noreply.github.com>
2021-06-30 23:27:46 +0200

    Fix Module name

--- UObject+Factory+EditorTools/MyEditorModuleNameOnlyModule/Private/MyEditorModuleNameModule.cpp
+++ UObject+Factory+EditorTools/MyEditorModuleNameOnlyModule/Private/MyEditorModuleNameModule.cpp
@@ -1,21 +1,21 @@
 // Created by Bionic Ape. All Rights Reserved.
 
-#include "BionicApeUIEditor.h"
+#include "MyEditorModuleName.h"
 #include "TypeActions/BAUIConfigTypeActions.h"
 #include "IAssetTools.h"
 
-#define LOCTEXT_NAMESPACE "FBionicApeUIEditorModule"
+#define LOCTEXT_NAMESPACE "FMyEditorModuleNameModule"
 
-void FBionicApeUIEditorModule::StartupModule()
+void FMyEditorModuleNameModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 	IAssetTools& AssetTools = FModuleManager::LoadModuleChecked<FAssetToolsModule>("AssetTools").Get();
 
-	EAssetTypeCategories::Type AssetCategoryBit = AssetTools.RegisterAdvancedAssetCategory(FName(TEXT("BionicApeUIEditor")), LOCTEXT("BionicApeUI", "BionicApeUI"));
+	EAssetTypeCategories::Type AssetCategoryBit = AssetTools.RegisterAdvancedAssetCategory(FName(TEXT("MyEditorModuleName")), LOCTEXT("BionicApeUI", "BionicApeUI"));
 	AssetTools.RegisterAssetTypeActions(MakeShareable(new FBAUIConfigTypeActions(AssetCategoryBit)));
 }
 
-void FBionicApeUIEditorModule::ShutdownModule()
+void FMyEditorModuleNameModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
@@ -23,4 +23,4 @@ void FBionicApeUIEditorModule::ShutdownModule()
 
 #undef LOCTEXT_NAMESPACE
 
-IMPLEMENT_MODULE(FBionicApeUIEditorModule, BionicApeUIEditor)
+IMPLEMENT_MODULE(FMyEditorModuleNameModule, MyEditorModuleName)
--- UObject+Factory+EditorTools/MyEditorModuleNameOnlyModule/Public/MyEditorModuleNameModule.h
+++ UObject+Factory+EditorTools/MyEditorModuleNameOnlyModule/Public/MyEditorModuleNameModule.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-class FBionicApeUIEditorModule : public IModuleInterface
+class FMyEditorModuleNameModule : public IModuleInterface
 {
 public:
 
