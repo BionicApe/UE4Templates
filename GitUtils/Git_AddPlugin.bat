cb097431eff93f00e7cdbf129e7091153fbd4b47
Bionic Ape <51401866+BionicApe@users.noreply.github.com>
2021-10-16 01:15:36 +0200

    Add Plugin set to main branch as default

--- GitUtils/Git_AddPlugin.bat
+++ GitUtils/Git_AddPlugin.bat
@@ -4,6 +4,7 @@ set /p ModName="Enter Plugin Name: "
 
 git submodule add https://github.com/BionicApe/%ModName% Plugins/%ModName%
 git submodule update --init --recursive
+git submodule foreach --recursive git checkout master
 git commit -m "Added new Plugin %ModName%"
 
 @echo off
